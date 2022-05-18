9ba9c9500d51c93e0f2672a6b235932d6a4b6418
Octavio Peri <octaperi@gmail.com>
2022-05-18 09:00:15 +0800

    Improve tests and names

--- robot.test.ts
+++ robot.test.ts
@@ -1,11 +1,20 @@
 let robots = require('./robot')
 let Robot = robots.Robot;
 
-test('valid position is greater than 0', () => {
+test('valid position is within a y-axis limit of 0', () => {
     var r = new Robot();
     expect(r.isValidPosition(2, -1)).toBe(false);
 });
-test('valid position is lesser than 5', () => {
+test('valid position is within a y-axis limit of 5', () => {
     var r = new Robot();
     expect(r.isValidPosition(1, 6)).toBe(false);
+});
+
+test('valid position is within a x-axis limit of 0', () => {
+    var r = new Robot();
+    expect(r.isValidPosition(-1, 3)).toBe(false);
+});
+test('valid position is within a x-axis limit of 5', () => {
+    var r = new Robot();
+    expect(r.isValidPosition(7, 2)).toBe(false);
 });
