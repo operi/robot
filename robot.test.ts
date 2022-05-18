eb7ce43273af60bb1640b50a600023502fb06656
Octavio Peri <octaperi@gmail.com>
2022-05-18 09:02:56 +0800

    Instantiate robot before each test

--- robot.test.ts
+++ robot.test.ts
@@ -1,19 +1,21 @@
 import { Robot } from './robot'
 
+var r = null;
+
+beforeEach(() => {
+    r = new Robot();
+})
+
 test('valid position is within a y-axis limit of 0', () => {
-    var r = new Robot();
     expect(r.isValidPosition(2, -1)).toBe(false);
 });
 test('valid position is within a y-axis limit of 5', () => {
-    var r = new Robot();
     expect(r.isValidPosition(1, 6)).toBe(false);
 });
 
 test('valid position is within a x-axis limit of 0', () => {
-    var r = new Robot();
     expect(r.isValidPosition(-1, 3)).toBe(false);
 });
 test('valid position is within a x-axis limit of 5', () => {
-    var r = new Robot();
     expect(r.isValidPosition(7, 2)).toBe(false);
 });
