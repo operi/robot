3f20f3ef59d03ec645cffc2c3d378ee8c2c9c58c
Octavio Peri <octaperi@gmail.com>
2022-05-18 10:12:49 +0800

    Improve test by parameterizing

--- robot.test.ts
+++ robot.test.ts
@@ -6,48 +6,26 @@ beforeEach(() => {
     r = new Robot();
 })
 
-test('valid position is within a y-axis limit of 0', () => {
-    expect(Robot.isValidPosition(2, -1)).toBe(false);
-});
-
-test('valid position is within a y-axis limit of 5', () => {
-    expect(Robot.isValidPosition(1, 6)).toBe(false);
-});
-
-test('valid position is within a x-axis limit of 0', () => {
-    expect(Robot.isValidPosition(-1, 3)).toBe(false);
-});
-
-test('valid position is within a x-axis limit of 5', () => {
-    expect(Robot.isValidPosition(7, 2)).toBe(false);
-});
-
-test('assert robot can move North', () => {
-    r.place(1, 1, FACING.NORTH);
-    r.move();
-    expect(r.getPositionX()).toBe(1);
-    expect(r.getPositionY()).toBe(2);
-});
-
-test('assert robot can move East', () => {
-    r.place(1, 1, FACING.EAST);
-    r.move();
-    expect(r.getPositionX()).toBe(2);
-    expect(r.getPositionY()).toBe(1);
-});
-
-test('assert robot can move South', () => {
-    r.place(2, 2, FACING.SOUTH);
-    r.move();
-    expect(r.getPositionX()).toBe(2);
-    expect(r.getPositionY()).toBe(1);
-});
-
-test('assert robot can move West', () => {
-    r.place(2, 2, FACING.WEST);
-    r.move();
-    expect(r.getPositionX()).toBe(1);
-    expect(r.getPositionY()).toBe(2);
+describe('Position Validation tests', () => {
+    it.each([
+        [2, -1, false],
+        [1, 6, false],
+        [-1, 3, false],
+        [7, 2, false]
+    ])('Is Position (%p , %p) valid', (x_position: number, y_position: number, expected_value: boolean) => {
+        expect(Robot.isValidPosition(x_position, y_position)).toBe(expected_value);
+    });
+    it.each([
+        [1, 1, FACING.NORTH, 1, 2],
+        [1, 1, FACING.EAST, 2, 1],
+        [2, 2, FACING.SOUTH, 2, 1],
+        [2, 2, FACING.WEST, 1, 2]
+    ])('assert robot from position (%p, %p) can move %p', (x_position: number, y_position: number, facing: FACING, expected_x: number, expected_y: number) => {
+        r.place(x_position, y_position, facing);
+        r.move();
+        expect(r.getPositionX()).toBe(expected_x);
+        expect(r.getPositionY()).toBe(expected_y);
+    });
 });
 
 test('assert robot can not cross North limit', () => {
